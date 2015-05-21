ec070172b316cd06e0377da6fac4278699faa9d4
joyousj <jsingharath82@gmail.com>
2015-05-21 18:21:22 -0500

    Create User.java

---
+++ User.java
@@ -0,0 +1,44 @@
+package com.dto;
+//Java beans
+
+public class User {
+	
+	private String userid;
+	private String password;
+	private String fname;
+	private String lname;
+	private String email;
+	
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	}
